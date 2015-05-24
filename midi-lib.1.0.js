9f70e6c1afb4014f6f8300365f5feb106af1c762
Tin Weasel <thetinweasel@gmail.com>
2015-05-24 13:35:51 +0000

    Adds missing files from WebMidi library refactor

---
+++ midi-lib.1.0.js
@@ -0,0 +1,115 @@
+// Library for MIDI interaction using Web MID API.
+// Specification at http://www.w3.org/TR/webmidi
+// --------------------------------------------------
+
+
+// Define namespace object for all library functions
+var WebMidi = WebMidi || {};
+
+
+// Library constants
+WebMidi.INIT_FAILURE = 0;
+WebMidi.INIT_SUCCESS = 1;
+
+WebMidi.SYSEX_NOT_REQUIRED = 0;
+WebMidi.SYSEX_REQUIRED = 1;
+
+// Create working 'static' object in namespace
+WebMidi.midi = function() {
+	
+	// Private class used for basic feedback in initialisation and setup of event callbacks.
+	var InitResult = function (result, message, data) {
+		// public members 
+		this.result = result || WebMidi.INIT_FAILURE;
+		this.message = message || 'Object not yet initialised';
+		this.data = data || {inputs: [], outputs: []};
+	};
+	
+	var MidiPort = function(id, name, state, isConnected) {
+		this.id = id;
+		this.name = name;
+		this.state = state;
+		this.isConnected = isConnected;
+	}
+	
+	// Scope var to hold user callback to invoke when we have a result.
+	var userCallback; // will call userCallback(InitResult) 
+	
+	// Request platform access to MIDI interface.
+	// Params: 
+	//		sysex - WebMidi.SYSEX_REQUIRED if access to MIDI sysex data required, WebMidi.SYSEX_NOT_REQUIRED otherwise
+	//		callback - user function called when response from platform is ready 
+	this.init = function (sysex, callback) {
+	
+		//build API options object from params
+		var options = { sysex: sysex == WebMidi.SYSEX_REQUIRED };
+		
+		//save user callback for later use
+		userCallback = callback;
+		
+		//call API which uses Promise-style callback handling
+		var promise;
+		try {
+			promise = navigator.requestMIDIAccess(); //TODO add options
+		} catch (error) {
+			this.returnError("API NOT SUPPORTED [" + error.message + "]");
+			return;
+		}
+		
+		if (!promise) {
+			this.returnError("INVALID API RESULT [No promise object returned]");
+			return;
+		}
+		
+		//with Promise objects, we can add handlers AFTER events have occurred and they will still fire - nice!
+		try {
+			promise.then(this.midiCallbackOk, this.midiCallbackFail);
+		} catch (error) {
+			this.returnError("FAILED ADDING PROMISE HANDLERS [" + error.message + "]");
+			return;
+		}
+	}
+	
+	// Callback invoked by API (via Promise) if request for MIDI access is successful.
+	// Params:
+	//		midiAccess - MIDIAccess object as defined by API. 
+	this.midiCallbackOk = function (midiAccess) {
+		
+		//alert("API Callback - OK");
+		console.log("API Callback - OK");
+		var access = midiAccess;
+		var inputPorts = [];
+		var outputPorts = [];
+		
+		//get list of all detected MIDI input ports
+		midiAccess.inputs.forEach( function(key,port) {
+			inputPorts.push(new MidiPort(port, key.name, key.state, key.connection));
+		});
+		
+		//get list of all detected MIDI output ports 
+		midiAccess.outputs.forEach( function(key,port) {
+			outputPorts.push(new MidiPort(port, key.name, key.state, key.connection));
+		});
+	
+		userCallback(new InitResult(WebMidi.INIT_SUCCESS, "API access successful!", { inputs: inputPorts, outputs: outputPorts}));	
+	}
+	
+	// Callback invoked by API (via promise) if request for MIDI access is unsuccessful.
+	// Params:
+	//		error - DOMException object as defined by API.
+	this.midiCallbackFail = function (error) {
+		//alert("API Callback - Fail");
+		console.log("API Callback - Fail");
+		var errstr = error.toString();
+		returnError(errstr);
+	}
+	
+	var returnError = function (error_string) {
+		//alert("Returning error to user: " + error_string);
+		console.log("Returning error to user: " + error_string);
+		userCallback(new InitResult(WebMidi.INIT_FAILURE, "API ACCESS FAILED [" + error_string + "]"));
+	}
+
+
+};
+
---
+++ webmiditest.js
@@ -0,0 +1,48 @@
+// Requires midi-lib.1.0.js
+
+
+function initApp() {
+    // start MIDI init and wait for result
+    var midi = new WebMidi.midi();
+    midi.init(WebMidi.SYSEX_NOT_REQUIRED, onFinished);
+}
+
+
+// Called when WebMidi has init results back from platform
+// Params:
+//      initResult - WebMidi.Result object send back from library
+function onFinished(initResult) {
+    
+    if (initResult.result == WebMidi.INIT_SUCCESS) {
+        //success
+        displayResult(initResult.message);
+        displayResult("<br />Detected ports are displayed below.");
+        
+        displayPorts("inputs",initResult.data.inputs);
+        displayPorts("outputs",initResult.data.outputs);
+    } else {
+        //failure
+        displayResult(initResult.message);
+    }
+}
+
+function displayResult(msg) {
+    document.getElementById("result").innerHTML += msg;
+}
+
+function displayPorts(parentId, ports) {
+
+    //create a new list UI element	
+	var list = document.createElement("ul");
+	var item;
+	ports.forEach( function(port) {
+		item = document.createElement("li");
+		item.appendChild(document.createTextNode("Port " + port.id + ": " + port.name + " [state: " + port.state + ", connection: " + port.connection + "]"));
+		list.appendChild(item);
+	});
+	
+	//append summary and new list to specified UI element
+	var div = document.getElementById(parentId);
+	div.appendChild(document.createTextNode("Number of ports: " + ports.length));
+	div.appendChild(list);
+}
